2df5461141a9d74a10759d0576c65164d43f4095
Даниил Клименков <klimenkokov1@gmail.com>
2025-09-20 16:27:42 +0300

    features : создание монолитного сервиса Auth

---
+++ Services/Auth/MVC.Auth.TimeCafe.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC.Auth.TimeCafe.API_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Services/Auth/MVC.Auth.TimeCafe.API/Models/PostmarkOptions.cs
@@ -0,0 +1,9 @@
+namespace MVC.Auth.TimeCafe.API_.Models
+{
+	public sealed class PostmarkOptions
+	{
+		public string? ServerToken { get; set; }
+		public string? FromEmail { get; set; }
+		public string? MessageStream { get; set; }
+	}
+} 
